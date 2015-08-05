d01efacdea50f57e4fe80780313939126544b167
Sam Dillard <samhdillard@gmail.com>
2015-08-05 00:37:10 +0000

    final without extra credit

--- bikes.py
+++ bikes.py
@@ -1,9 +1,9 @@
 class Bicycle(object):
-    def __init__(self, name, weight, cost, price):
+    def __init__(self, name, weight, cost):
         self.name = name
         self.weight = weight
         self.cost = cost
-        self.price = price
+
     def __repr__(self): 
         return "Bicycle(%r, %r, %r)" % (self.name, self.weight, self.cost)
     
@@ -16,10 +16,13 @@ class Shop(object):
     def addInventory(self, bike):
         self.inventory.append(bike)
         
-    def sell(self, cost, price):
-        self.price = cost * self.markup
-        margin = price - cost
-        return margin
+    def pricing(self, bicycle):
+        price = bicycle.cost * self.markup
+        return price
+        
+    def sell(self, bicycle):
+        profit = self.pricing(bicycle) - bicycle.cost
+        print "profit is {}".format(profit)
     
     
     # def totalProfit(self, margin):
@@ -36,12 +39,12 @@ class Customer(object):
         self.bikes.append(bike)
             
 
-myBike = Bicycle("Schwinn", 24, 200, 250)
-myBike1 = Bicycle("Torpedo", 24, 200, 300)
-myBike2 = Bicycle("Schwinny",30, 250, 350)
-myBike3 = Bicycle("Speedy", 20, 100, 175)
-myBike4 = Bicycle("Speedy2", 15, 100, 150)
-myBike5 = Bicycle("Torpy", 45, 300, 500)
+myBike = Bicycle("Schwinn", 24, 200)
+myBike1 = Bicycle("Torpedo", 24, 200)
+myBike2 = Bicycle("Schwinny",30, 250)
+myBike3 = Bicycle("Speedy", 20, 100)
+myBike4 = Bicycle("Speedy2", 15, 100)
+myBike5 = Bicycle("Torpy", 45, 300)
 
 bicycles = [myBike,myBike1,myBike2,myBike3,myBike4,myBike5]
 
@@ -59,7 +62,7 @@ customers = [bill,sam,jared]
 def afford(customer):
     canAfford = []
     for x in bicycles:
-        if customer.fund >= x.price:
+        if customer.fund >= shop.pricing(x):
            canAfford.append(x) 
            
     return canAfford 
@@ -72,7 +75,8 @@ def afford(customer):
 #           canAfford.append(bicycles[x])           
 #    return canAfford
     
-
-    
-print afford(sam)
+for customer in customers:
+    affordList = afford(customer)
+    shop.sell(affordList[0])
+    print customer.name
 
